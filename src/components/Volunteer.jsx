708f783135fb167427a5145c95a067f9d6df578d
Ayomidemi <93412345+Ayomidemi@users.noreply.github.com>
2022-06-24 23:40:10 +0100

    fake api calls

--- src/components/Volunteer.jsx
+++ src/components/Volunteer.jsx
@@ -1,34 +1,43 @@
-import React, {useState} from "react";
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Volunteer = () => {
-
   const [volunteer, setVolunteer] = useState({
-    
-  })
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+    dob: "",
+    skills: "",
+    salvationStory: "",
+    volunteer: "",
+  });
 
   const handleInputChange = (e) => {
-    setVolunteer( (previousDetails) => {
-      return {...previousDetails, [e.target.name]: e.target.value}
-    }) 
-  }
+    setVolunteer((previousDetails) => {
+      return { ...previousDetails, [e.target.name]: e.target.value };
+    });
+  };
 
   const volunteerForm = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    axios.post('http://127.0.0.1:8000/volunteer/api/volunteer-create', volunteer)
-    .then( (res) => {
-      console.log(res.data)
-    })
-    .catch( (err) => {
-      console.log(err)
-    })
-  }
-
-
+    axios
+      .post(
+        "https://peniel-server.herokuapp.com/volunteer/api/volunteer-create",
+        volunteer
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div className="volunteer" >
+    <div className="volunteer">
       <div className="volunteer-banner">
         <h1>Volunteer </h1>
       </div>
@@ -43,46 +52,52 @@ const Volunteer = () => {
           <form action="/action_page.php" onSubmit={volunteerForm}>
             <label htmlFor="fname">Name</label>
             <input
-                  onChange={handleInputChange}
+              onChange={handleInputChange}
               type="text"
               className="contact name"
-              id="fname"
-              name="fname"
-              placeholder="First name"
+              name="firstName"
+              value={volunteer.firstName}
               required
             />
             <input
-                  onChange={handleInputChange}
+              onChange={handleInputChange}
               type="text"
               className="contact name"
-              id="lname"
-              name="lname"
+              name="lastName"
+              value={volunteer.lastName}
               placeholder="Last name"
               required
             />
 
             <label htmlFor="email">Email</label>
             <input
-                  onChange={handleInputChange}
+              onChange={handleInputChange}
               type="text"
               className="contact name"
               name="email"
+              value={volunteer.email}
               placeholder="Email Address"
               required
             />
 
             <label htmlFor="pnumber">Phone Number</label>
             <input
-                  onChange={handleInputChange}
-                  type="tel"  id="phone" placeholder="Phone Number" className="contact name" required/>
-            
+              onChange={handleInputChange}
+              value={volunteer.phoneNumber}
+              type="tel"
+              name="phoneNumber"
+              placeholder="Phone Number"
+              className="contact name"
+              required
+            />
 
             <label htmlFor="address">Address</label>
             <input
-                  onChange={handleInputChange}
+              onChange={handleInputChange}
               type="text"
               className="contact name"
               name="address"
+              value={volunteer.address}
               placeholder="Address"
               required
             />
@@ -90,36 +105,54 @@ const Volunteer = () => {
             <div className="volunteer-form-flex">
               <div className="volunteer-flex-1">
                 <label htmlFor="dob">Date of Birth</label>
-            <input
+                <input
                   onChange={handleInputChange}
-                  type="date"  name="date" className="contact name" required/>
+                  value={volunteer.dob}
+                  type="date"
+                  name="dob"
+                  className="contact name"
+                  required
+                />
               </div>
 
               <div className="volunteer-flex-2">
                 <label htmlFor="dob">Skills</label>
-            <select 
+                <select
                   onChange={handleInputChange}
-                  id="department" name="department" className="contact name">
-              <option value="department">Departments</option>
-              <option value="department">Prison Outreach</option>
-              <option value="department">Street Outreach</option>
-              <option value="department">Photography</option>
-              <option value="department">Grants / Proposal Writing</option>
-              <option value="department">Communication</option>
-            </select>
+                  value={volunteer.skills}
+                  id="department"
+                  name="department"
+                  className="contact name"
+                >
+                  <option value="department">Departments</option>
+                  <option value="prison outreach">Prison Outreach</option>
+                  <option value="street outreach">Street Outreach</option>
+                  <option value="photography">Photography</option>
+                  <option value="grants / proposal writing">
+                    Grants / Proposal Writing
+                  </option>
+                  <option value="communication">Communication</option>
+                </select>
               </div>
-        
             </div>
 
             <label htmlFor="message">What is your salvation story?</label>
             <textarea
-                  onChange={handleInputChange}
-                  name="message" id="message" required></textarea>
+              onChange={handleInputChange}
+              name="message"
+              value={volunteer.salvationStory}
+              id="message"
+              required
+            ></textarea>
 
             <label htmlFor="message">Why do you want to volunteer?</label>
             <textarea
-                  onChange={handleInputChange}
-                  name="message" id="message" required></textarea>
+              onChange={handleInputChange}
+              name="message"
+              value={volunteer.volunteer}
+              id="message"
+              required
+            ></textarea>
 
             <button className="volunteer-button">Submit</button>
           </form>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,38 @@
-import React, {useState} from "react";
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Contact = () => {
-
   const [contact, setContact] = useState({
-
-  })
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+    message: "",
+  });
+  console.log(contact);
 
   const handleInputChange = (e) => {
-    setContact( (previousDetails) => {
-      return {...previousDetails, [e.target.name]: e.target.value}
-    }) 
-  }
+    setContact((previousDetails) => {
+      return { ...previousDetails, [e.target.name]: e.target.value };
+    });
+  };
 
   const contactForm = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    axios.post('http://127.0.0.1:8000/contact/api/contact-create', contact)
-    .then( (res) => {
-      console.log(res.data)
-    })
-    .catch( (err) => {
-      console.log(err)
-    })
-  }
+    axios
+      .post(
+        "https://peniel-server.herokuapp.com/contact/api/contact-create",
+        contact
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="volunteer" id="navbar">
@@ -34,65 +43,72 @@ const Contact = () => {
       <div className="volunteer-container">
         <div className="volunteer-form">
           <h2>
-            Want to chat? We'd love to hear from you! Get in touch with
-            us and also get your questions answered.
+            Want to chat? We'd love to hear from you! Get in touch with us and
+            also get your questions answered.
           </h2>
 
           <form action="/action_page.php" onSubmit={contactForm}>
             <label htmlFor="fname">Name</label>
             <input
-                  onChange={handleInputChange}
+              onChange={handleInputChange}
               type="text"
               className="contact name"
-              id="fname"
-              name="fname"
+              name="firstName"
+              value={contact.firstName}
               placeholder="First Name"
               required
             />
             <input
-                  onChange={handleInputChange}
+              onChange={handleInputChange}
               type="text"
               className="contact name"
-              id="lname"
-              name="lname"
+              name="lastName"
+              value={contact.lastName}
               placeholder="Last Name"
               required
             />
 
             <label htmlFor="email">Email</label>
             <input
-                  onChange={handleInputChange}
+              onChange={handleInputChange}
               type="text"
               className="contact name"
               name="email"
+              value={contact.email}
               placeholder="Email Address"
               required
             />
 
             <label htmlFor="pnumber">Phone Number</label>
             <input
-                  onChange={handleInputChange}
+              onChange={handleInputChange}
               type="number"
               className="contact name"
-              id="pnumber"
+              name="phoneNumber"
+              value={contact.phoneNumber}
               placeholder="Phone Number"
               required
             />
 
             <label htmlFor="address">Address</label>
             <input
-                  onChange={handleInputChange}
+              onChange={handleInputChange}
               type="text"
               className="contact name"
               name="address"
+              value={contact.address}
               placeholder="Address"
               required
             />
 
             <label htmlFor="message">What would you like to discuss?</label>
             <textarea
-                  onChange={handleInputChange}
-                  name="message" id="message" required></textarea>
+              onChange={handleInputChange}
+              value={contact.message}
+              name="message"
+              id="message"
+              required
+            ></textarea>
 
             <button className="volunteer-button">Submit</button>
           </form>
